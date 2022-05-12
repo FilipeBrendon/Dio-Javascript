c3dc24409415fc621e2649604ad887af2ca72394
Filipe Brendon <103082073+FilipeBrendon@users.noreply.github.com>
2022-05-12 16:13:07 -0300

    DOM

--- 5.Map, Filter e Reduce/playground5.js
+++ 5.Map, Filter e Reduce/playground5.js
@@ -19,7 +19,7 @@ const saldoDisponivel = 100
 
 function calculaSaldo(saldoDisponivel, lista){
     return lista.reduce(function(prev, current, index){
-        console.log("rodada", index + 1);
+        console.log("rodada", index ++);
         console.log({prev});
         console.log({current});
         return prev - current.preco
